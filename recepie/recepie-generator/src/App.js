5201567ff0d06bae0c0d76e0161ecb027de33763
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-04-25 20:40:14 -0400

    Implemented Functionality to add base ingredients

--- recepie/recepie-generator/src/App.js
+++ recepie/recepie-generator/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import uuid from "react-uuid";
 import BaseIngredients from "./BaseIngredients";
 import MainIngredients from "./MainIngredients";
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+  const onAddIngredient = () => {
+    console.log(ingredients);
+    const newIngredients = {
+      id: uuid(),
+      title: "Random Ingredient",
+    };
+    setIngredients([newIngredients, ...ingredients]);
+  };
   return (
     <div className="App">
-      <BaseIngredients />
+      <BaseIngredients
+        ingredients={ingredients}
+        onAddIngredient={onAddIngredient}
+      />
       <MainIngredients />
     </div>
   );
--- recepie/recepie-generator/src/BaseIngredients.js
+++ recepie/recepie-generator/src/BaseIngredients.js
@@ -1,18 +1,20 @@
-function BaseIngredients() {
+function BaseIngredients({ ingredients, onAddIngredient }) {
   return (
     <>
       <div className="sidebar">
         <div className="app-sidebar-header">
           <h1>Base Ingredients</h1>
-          <button>Add</button>
+          <button onClick={onAddIngredient}>Add</button>
         </div>
         <div className="app-sidebar-ing">
-          <div className="app-sidebar-items">
-            <div className="sidebar-note-title">
-              <strong>Cheese</strong>
-              <button>Delete</button>
+          {ingredients.map((ingredient) => (
+            <div className="app-sidebar-items">
+              <div className="sidebar-note-title">
+                <strong>{ingredient.title}</strong>
+                <button>Delete</button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
