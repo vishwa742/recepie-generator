24e4e11ed0f5a3d181dc822e768336655cffaa7b
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-04-26 19:10:15 -0400

    Update ImageLoader.js

--- recepie/recepie-generator/src/ImageLoader.js
+++ recepie/recepie-generator/src/ImageLoader.js
@@ -12,24 +12,24 @@ function ImageLoader() {
   const [store, setStore] = useState("");
   const [words, setWords] = useState([]);
   var tester = [];
-
-  const removedWords = [
-    "Product",
-    "aty:",
-    "Z Total:",
-    "Order",
-    "Summary item",
-    "item",
-    "Subtotat",
-    "Sales",
-    "Tax",
-    "Total",
-    "Total:",
-    "best",
-  ];
+  const items = words.toString();
+  const [recepie, setRecepie] = useState([]);
 
   const onDrop = (_, pictureUrl) => {
     setPicUrl(pictureUrl);
+  };
+
+  const getRecepies = () => {
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=4b4353c06ae445229cad091a7b2abf34&ingredients=` +
+        items +
+        `&number=15&ranking=2`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //console.log(words);
+      });
   };
 
   const runOcr = () => {
@@ -37,17 +37,12 @@ function ImageLoader() {
       Tesseract.recognize(picture, "eng").then(({ data: { text } }) => {
         text = text.replace(/[0-9]/g, "");
         text = text.replace(/[$.,]/g, "");
-
         const textSting = text.replace(" ", ",");
-
         text = text.split(" ");
-
         const wordFiltered = new Set(Array.from(text));
-        //console.log(wordFiltered);
         setWords(...words, wordFiltered);
         setWords((oldArray) => [...oldArray, words]);
-        //setStore(words.filter((word) => word.length > 3));
-        console.log(words);
+        //console.log(words);
         setOcrText((oldArray) => [...oldArray, wordFiltered]);
       })
     );
@@ -81,17 +76,17 @@ function ImageLoader() {
             {words.map((ot) => {
               console.log(ot);
               if (
-                ot != "Product" &&
-                ot != "aty:" &&
-                ot != "Z Total:" &&
-                ot != "Summary item" &&
-                ot != "Sales" &&
-                ot != "item" &&
-                ot != "Subtotat" &&
-                ot != "Sales" &&
-                ot != "Tax" &&
-                ot != "Total:" &&
-                ot != "Order"
+                ot !== "Product" &&
+                ot !== "aty:" &&
+                ot !== "Z Total:" &&
+                ot !== "Summary item" &&
+                ot !== "Sales" &&
+                ot !== "item" &&
+                ot !== "Subtotat" &&
+                ot !== "Sales" &&
+                ot !== "Tax" &&
+                ot !== "Total:" &&
+                ot !== "Order"
               ) {
                 return <div>{ot}</div>;
               }
@@ -100,6 +95,8 @@ function ImageLoader() {
         ) : (
           <ClipLoader color="#ffffff" loading={isLoading} size={150} />
         )}
+        {/* {recepie.length > 0 && <div>Hello {recepie.title}</div>} */}
+        <button onClick={getRecepies}>Click me</button>
       </div>
     </>
   );
