fb626d29f992e28aef3c8c3853ae2c7411719be9
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-04-25 20:17:51 -0400

    updated layout

--- recepie/recepie-generator/src/BaseIngredients.js
+++ recepie/recepie-generator/src/BaseIngredients.js
@@ -1,7 +1,20 @@
 function BaseIngredients() {
   return (
     <>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <div className="app-sidebar-header">
+          <h1>Base Ingredients</h1>
+          <button>Add</button>
+        </div>
+        <div className="app-sidebar-ing">
+          <div className="app-sidebar-items">
+            <div className="sidebar-note-title">
+              <strong>Cheese</strong>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
